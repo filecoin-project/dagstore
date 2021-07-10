12449ab407ba013e910d3fdd6a3380c138aa85a1
Raúl Kripalani <raul@protocol.ai>
2021-07-10 23:29:09 +0100

    solve lock contention between shard persistence and upgrader.

--- mount/upgrader.go
+++ mount/upgrader.go
@@ -17,13 +17,14 @@ import (
 // If the underlying mount is fully-featured, the Upgrader has no effect, and
 // simply passes through to the underlying mount.
 type Upgrader struct {
+	rootdir     string
 	underlying  Mount
-	passthrough bool
 	key         string
+	passthrough bool
 
 	lk        sync.Mutex
 	transient string
-	rootdir   string
+	once      *sync.Once
 }
 
 var _ Mount = (*Upgrader)(nil)
@@ -32,7 +33,7 @@ var _ Mount = (*Upgrader)(nil)
 // will reuse the file in path `initial` as the initial transient copy. Whenever
 // a new transient copy has to be created, it will be created under `rootdir`.
 func Upgrade(underlying Mount, rootdir, key string, initial string) (*Upgrader, error) {
-	ret := &Upgrader{underlying: underlying, key: key, rootdir: rootdir}
+	ret := &Upgrader{underlying: underlying, key: key, rootdir: rootdir, once: new(sync.Once)}
 	if ret.rootdir == "" {
 		ret.rootdir = os.TempDir() // use the OS' default temp dir.
 	}
@@ -61,20 +62,30 @@ func (u *Upgrader) Fetch(ctx context.Context) (Reader, error) {
 	}
 
 	// determine if the transient is still alive.
+	// if not, get the current sync.Once and trigger a refresh.
+	// after it's done, open the resulting transient.
 	u.lk.Lock()
-	defer u.lk.Unlock()
-
 	if u.transient != "" {
 		if _, err := os.Stat(u.transient); err == nil {
+			u.lk.Unlock()
 			return os.Open(u.transient)
 		}
 		// TODO add size check.
 	}
-
 	// transient appears to be dead, refetch.
-	if err := u.refetch(ctx); err != nil {
-		return nil, err
+	// get the current sync under the lock, use it to deduplicate concurrent fetches.
+	once := u.once
+	u.lk.Unlock()
+
+	var err error
+	once.Do(func() { err = u.refetch(ctx) })
+
+	if err != nil {
+		return nil, fmt.Errorf("mount fetch failed: %w", err)
 	}
+
+	u.lk.Lock()
+	defer u.lk.Unlock()
 
 	return os.Open(u.transient)
 }
@@ -120,16 +131,17 @@ func (u *Upgrader) Close() error {
 }
 
 func (u *Upgrader) refetch(ctx context.Context) error {
+	u.lk.Lock()
 	if u.transient != "" {
 		_ = os.Remove(u.transient)
 	}
+	u.lk.Unlock()
+
 	file, err := os.CreateTemp(u.rootdir, "transient-"+u.key+"-*")
 	if err != nil {
 		return fmt.Errorf("failed to create temporary file: %w", err)
 	}
 	defer file.Close()
-
-	u.transient = file.Name()
 
 	// sanity check on underlying mount.
 	if stat, err := u.underlying.Stat(ctx); err != nil {
@@ -149,6 +161,12 @@ func (u *Upgrader) refetch(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed to copy underlying mount to transient file: %w", err)
 	}
+
+	// set the new transient path under a lock, and recycle the sync.Once.
+	u.lk.Lock()
+	u.transient = file.Name()
+	u.once = new(sync.Once)
+	u.lk.Unlock()
 
 	return nil
 }
