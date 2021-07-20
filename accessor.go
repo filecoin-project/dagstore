ba2befcdd9bf1001996da58cc51ba702332dc8e3
Masih H. Derkani <m@derkani.org>
2021-07-20 19:42:17 +0100

    Enable `UseWholeCIDs` since by default CARv2 uses multihash
    Explicitly set the option `UseWholeCIDs` to ture, since v2 uses
    multihash by default.

--- accessor.go
+++ accessor.go
@@ -70,7 +70,7 @@ func (sa *ShardAccessor) Blockstore() (ReadBlockstore, error) {
 	}
 	sa.lk.Unlock()
 
-	bs, err := blockstore.NewReadOnly(r, sa.idx, carv2.ZeroLengthSectionAsEOF(true))
+	bs, err := blockstore.NewReadOnly(r, sa.idx, carv2.ZeroLengthSectionAsEOF(true), blockstore.UseWholeCIDs(true))
 	return bs, err
 }
 
