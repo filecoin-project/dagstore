4caadf89b7e6e314201a8bc3695a1b4c18597bcd
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-09-14 15:38:07 +0300

    fixup

--- dagstore_control.go
+++ dagstore_control.go
@@ -67,7 +67,7 @@ func (d *DAGStore) control() {
 		var span trace.Span
 		if d.tracer != nil {
 			d.ctx, span = d.tracer.Start(d.ctx, "dagstore.control.consumeNext")
-			span.SetAttributes(attribute.String("shardkey", tsk.shard.key))
+			span.SetAttributes(attribute.String("shardkey", tsk.shard.key.String()))
 		}
 
 		s.lk.Lock()
